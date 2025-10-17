b125ef7d47fc9e30ad4c65f4be22ddf85dadedc9
Artur <artur.matkowski.zan@gmail.com>
2025-10-17 15:17:28 +0200

    fx: added missing @rendermode InteractiveServer for psql writes

--- Services/StudentService.cs
+++ Services/StudentService.cs
@@ -7,8 +7,14 @@ public class StudentService
     {
         _dbcontext = dbcontext;
     }
-    public  List<StudentClass> GetStudents()
+    public List<StudentClass> GetStudents()
     {
         return _dbcontext.student.ToList();
+    }
+    
+    public void AddStudent(StudentClass _student)
+    {
+        _dbcontext.student.Add(_student);
+        _dbcontext.SaveChanges();
     }
 }
