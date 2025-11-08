09fc469eea93364ac02e3fb91e9cb46ac1f9a626
Artur <artur.matkowski.zan@gmail.com>
2025-11-08 15:42:44 +0100

    feat: add creation timestamp default to user account model and migration

---
+++ Migrations/20251108140654_AddCreationTimestampDefault.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Konfucjusz.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreationTimestampDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "creation_timestamp",
+                table: "user_account",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "creation_timestamp",
+                table: "user_account",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "now()");
+        }
+    }
+}
